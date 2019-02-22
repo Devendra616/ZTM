ed6334a6ebddb1fcdb65549fa7b4047998b2bea5
Devendra Singh <DEVENDRA@Dev-Lap.nmdc.co.in>
2019-02-23 01:45:55 +0530

    getting data from API removed hard coded robots

--- React/robofriends/src/App.js
+++ React/robofriends/src/App.js
@@ -7,9 +7,16 @@ import './App.css';
 class App extends Component {
     constructor(){
         super();
-        this.state= {robots:robots,
+        this.state= {robots:[],
                      searchField:""
                     };
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots:robots}));
+        
     }
 
     onSearchChange= (event)=> { //user created function
@@ -21,13 +28,19 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         });
 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots }/ >
-            </div>
-        );
+        if(this.state.robots.length ===0 ){
+            return <h1>Loading ...</h1>
+        }else{
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots }/ >
+                </div>
+            );
+        }
+
+        
     }
 }
 
