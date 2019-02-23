9048370730f2a5e3d52d85724f4a10045b5d3a27
Devendra Singh <DEVENDRA@Dev-Lap.nmdc.co.in>
2019-02-24 02:01:06 +0530

    build the project

---
+++ React/robofriends/build/precache-manifest.71c88cd2690ee6f98e9727fd19e4f018.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "57ad6cb1828e85f5f029d166ac0361b8",
+    "url": "/static/media/SEGA-LOGO-FONT.ttf.57ad6cb1.eot"
+  },
+  {
+    "revision": "3f5c9111609279eb69be9e1a8a10e82b",
+    "url": "/static/media/SEGA-LOGO-FONT.ttf.3f5c9111.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c6ec1e91653d49b75d25",
+    "url": "/static/js/main.c6ec1e91.chunk.js"
+  },
+  {
+    "revision": "82e7610eae1127e95aee",
+    "url": "/static/js/2.82e7610e.chunk.js"
+  },
+  {
+    "revision": "c6ec1e91653d49b75d25",
+    "url": "/static/css/main.50a0428a.chunk.css"
+  },
+  {
+    "revision": "82e7610eae1127e95aee",
+    "url": "/static/css/2.659652a2.chunk.css"
+  },
+  {
+    "revision": "83670addd1371b3c1a33fe35aab40d7b",
+    "url": "/index.html"
+  }
+];
